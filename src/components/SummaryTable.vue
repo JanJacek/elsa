bf4728bf773627e97f7818ad604b080d43b0646f
JanJacek <bobkiewicz.jacek@gmail.com>
2023-02-16 19:07:13 +0100

    add summary tatble

--- src/components/SummaryTable.vue
+++ src/components/SummaryTable.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <p>{{ s_store.counter }}</p>
+  <div class="q-ma-md">
+    <q-table
+      title="Treats"
+      :rows="s_store.getRows"
+      :columns="s_store.getColumns"
+      row-key="name"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+//libraries
 //stores
 import { summaryStore } from 'src/stores/summaryStore';
+//types
 /* script */
 const s_store = summaryStore();
 </script>
