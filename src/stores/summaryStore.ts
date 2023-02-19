7ddb866619d07922561a24b44012e3028243b5f2
JanJacek <bobkiewicz.jacek@gmail.com>
2023-02-19 20:20:20 +0100

    Prepare data

--- src/stores/summaryStore.ts
+++ src/stores/summaryStore.ts
@@ -1,19 +1,49 @@
 //libraries
 import { defineStore } from 'pinia';
 // types
-import {
-  SummaryJson,
-  Row,
-  TableColumn,
-} from './storesModels/summaryStoreModels';
+import { Row, TableColumn } from './storesModels/summaryStoreModels';
 
 export const summaryStore = defineStore('summaryStore', {
   state: (): {
-    summaryJson: SummaryJson;
+    dictionary: Record<string, Row>;
+    groupLevel: {
+      label: string;
+      value: number;
+    }[];
   } => ({
-    summaryJson: {
-      columns: {
+    dictionary: {},
+    groupLevel: [
+      {
+        label: 'BE1',
+        value: 110,
+      },
+      {
+        label: 'BE2',
+        value: 124,
+      },
+      {
+        label: 'BE3',
+        value: 138,
+      },
+      {
+        label: 'BE4',
+        value: 165,
+      },
+      {
+        label: 'BE5',
+        value: 194,
+      },
+      {
+        label: 'BE6',
+        value: 222,
+      },
+    ],
+  }),
+  getters: {
+    getColumns(): TableColumn[] {
+      const columns = {
         name: 'Name',
+        comment: 'Comment',
         assignmentsCompleted: 'Assignments Completed',
         assignmentsAvgScore: 'Assignments Avg Score',
         assignmentsAvgGrade: 'Assignments Avg Grade',
@@ -21,14 +51,9 @@ export const summaryStore = defineStore('summaryStore', {
         testsAvgGrade: 'Tests Avg Grade',
         oralVocabulary: 'Oral Vocabulary',
         oralInteraction: 'Oral Interaction',
-      },
-      rows: [],
-    },
-  }),
-  getters: {
-    getColumns(): TableColumn[] {
+      };
       const columnsForTable: TableColumn[] = [];
-      for (const [key, val] of Object.entries(this.summaryJson.columns)) {
+      for (const [key, val] of Object.entries(columns)) {
         columnsForTable.push({
           name: key,
           label: val,
@@ -41,26 +66,28 @@ export const summaryStore = defineStore('summaryStore', {
     },
     getRows(): Row[] {
       const rowsForTable: Row[] = [];
-      for (const row of this.summaryJson.rows) {
+      Object.keys(this.dictionary).forEach((key) => {
         rowsForTable.push({
-          name: row.name,
-          username: row.username,
-          lastLogin: row.lastLogin,
-          enrolmentDate: row.enrolmentDate,
-          assignmentsCompleted: row.assignmentsCompleted,
-          assignmentsAvgScore: row.assignmentsAvgScore,
-          assignmentsAvgGrade: row.assignmentsAvgGrade,
-          testsCompleted: row.testsCompleted,
-          testsAvgScore: row.testsAvgScore,
-          testsAvgGrade: row.testsAvgGrade,
-          practicesCompleted: row.practicesCompleted,
-          practicesAvgScore: row.practicesAvgScore,
-          practicesAvgGrade: row.practicesAvgGrade,
-          totalTimeOnTask: row.totalTimeOnTask,
-          oralVocabulary: row.oralVocabulary,
-          oralInteraction: row.oralInteraction,
+          name: this.dictionary[key].name,
+          username: this.dictionary[key].username,
+          lastLogin: this.dictionary[key].lastLogin,
+          enrolmentDate: this.dictionary[key].enrolmentDate,
+          assignmentsCompleted: this.dictionary[key].assignmentsCompleted,
+          assignmentsAvgScore: this.dictionary[key].assignmentsAvgScore,
+          assignmentsAvgGrade: this.dictionary[key].assignmentsAvgGrade,
+          testsCompleted: this.dictionary[key].testsCompleted,
+          testsAvgScore: this.dictionary[key].testsAvgScore,
+          testsAvgGrade: this.dictionary[key].testsAvgGrade,
+          practicesCompleted: this.dictionary[key].practicesCompleted,
+          practicesAvgScore: this.dictionary[key].practicesAvgScore,
+          practicesAvgGrade: this.dictionary[key].practicesAvgGrade,
+          totalTimeOnTask: this.dictionary[key].totalTimeOnTask,
+          oralVocabulary: this.dictionary[key].oralVocabulary,
+          oralInteraction: this.dictionary[key].oralInteraction,
+          description: this.dictionary[key].description,
+          id: key,
         });
-      }
+      });
       return rowsForTable;
     },
   },
